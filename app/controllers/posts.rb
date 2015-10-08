499fc2321b31ac68f5ea9fb739df5363659cb58b
smokecode <smokecode@gmail.com>
2015-10-08 12:00:52 -0400

    routes

---
+++ app/controllers/posts.rb
@@ -0,0 +1,34 @@
+get '/' do
+  @posts = Post.all
+  erb :'/posts/index'
+end
+
+get '/post/new' do
+  erb :'/posts/new'
+end
+
+post '/post/new' do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect ('/')
+  else
+    @post.errors.messages
+  end
+end
+
+post '/post/:id/comment' do
+  binding.pry
+  @comment = Comment.new(params[:comment])
+  @post = Post.find(params[:id])
+  @post.comments << @comment
+  if @post.save
+    redirect ('/post/:id')
+  else
+    @post.errors.messages
+  end
+end
+
+get '/post/:id' do
+  @post = Post.find(params[:id])
+  erb :'/posts/show'
+end
