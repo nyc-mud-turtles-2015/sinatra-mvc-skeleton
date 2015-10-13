2402d7f5140053f12decc975725b2b13f0bdb6e9
smokecode <smokecode@gmail.com>
2015-10-13 11:15:53 -0400

    relations fixed for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :url, presence: true, format: {with: /http(|s):\/\/.+\.(com|org|net|gov)/,
       message: "your url should include http prefix and proper domain suffix"}
   belongs_to :user
-  has_many :comments, through: :user, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
 
   has_many :votes, :as => :voteable, :dependent => :destroy
 end
